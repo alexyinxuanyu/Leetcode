80c8b1ed4aa637020a764b7f7630aa99a4da2fd2
alexyinxuanyu <37372050+alexyinxuanyu@users.noreply.github.com>
2018-08-06 20:51:24 +0900

    Add files via upload
    leetcode 804

---
+++ forin.py
@@ -0,0 +1,17 @@
+morses = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+morse=['1','2','3']
+for word in morse:
+    for w in word:
+        print(w)
+
+a=[]
+
+a.append(''.join("dsa"))
+print(a)
+a.append(''.join("!!"))
+print(a)
+a.append(''.join("dsa"))
+print(a)
+print (len(set(a)))
+
+#use the set can remove the assemble of list
